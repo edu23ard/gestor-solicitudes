aca70eb2e913e93bb311426cb764d290d489f623
edu23ard <e.2.d.3.u.ard@gmail.com>
2026-03-07 18:34:11 -0600

    Proyecto DAA Unidad 2

---
+++ app/register/page.tsx
@@ -0,0 +1,15 @@
+export default function RegisterPage() {
+
+ return (
+
+   <main style={{ padding: "2rem" }}>
+
+     <h1>Registrar de usuario</h1>
+
+     <p>Pantalla para crear una nueva cuenta de usuario.</p>
+
+   </main>
+
+ );
+
+}
